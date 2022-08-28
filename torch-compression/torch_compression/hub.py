0cca19bf0c78f5266579a1dfdf76dffb3dc2ad28
tl32rodan <tl32rodan.cs09g@nctu.edu.tw>
2022-08-28 22:40:51 +0800

    Update

--- torch-compression/torch_compression/hub.py
+++ torch-compression/torch_compression/hub.py
@@ -9057,7 +9057,7 @@ __CODER_TYPES__ = {"GoogleFactorizedCoder": GoogleFactorizedCoder, "GoogleIDFPri
                    "CondANFPredPriorDCVCBackbone": CondANFPredPriorDCVCBackbone,
                    "CondANFPredPriorDCVCBackboneCustom1": CondANFPredPriorDCVCBackboneCustom1,
                    "CondANFPredPriorDCVCBackboneCustom2": CondANFPredPriorDCVCBackboneCustom2,
-                   "AsymmetricCANFPredPrior": AsymmetricCANFPredPrior,
+                   #"AsymmetricCANFPredPrior": AsymmetricCANFPredPrior,
                   }
 
 
